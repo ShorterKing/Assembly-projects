048efc9d4ab820bdf83f58b48542bf606535684b
AKSHAT Singh <84120105+ShorterKing@users.noreply.github.com>
2022-09-23 19:55:35 +0530

    Add files via upload
    In this assembly code, we learn how to find the greatest number among the 10. Using loop

---
+++ All the projects CODES/Greatest number among 10.asm
@@ -0,0 +1,33 @@
+CODE SEGMENT
+          ASSUME CS:CODE 
+          START:
+          MOV AX,2000H
+          MOV DS,AX
+          MOV [0000H],011H
+          MOV [0001H],05BH
+          MOV [0002H],0AAH
+          MOV [0003H],0A2H
+          MOV [0004H],0FEH
+          MOV [0005H],0ABH
+          MOV [0006H],0AEH
+          MOV [0007H],0BFH
+          MOV [0008H],0EFH
+          MOV [0009H],0FFH
+          MOV [000AH],0AFH
+          MOV CH,00H
+          MOV SI,0000H
+          MOV CL,[SI]
+          INC SI
+          MOV AL,[SI]
+          INC SI
+  L1:     CMP AL, [SI]
+          JNC SKIP
+          MOV AL, [SI]  
+          SKIP:   INC SI  
+          LOOP L1      
+          MOV [SI], AL
+          MOV AH,4CH 
+          INT 21H    
+          CODE ENDS
+END START
+          
